f360ed534a336f73fd3a305358b4d02fab39efe5
Richard Bock <richard.bock@rbock44.de>
2019-06-19 01:18:27 +0200

    ADD: aggregator.sql support

--- aggregator/cron.sh
+++ aggregator/cron.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "run initial aggregation at the beginning"
+/tmp/run
+
 echo "start with cron pattern [${CRON_PATTERN}]"
 
 /bin/sh -c "echo \"${CRON_PATTERN} /tmp/run\" | crontab - && crond -f -L /dev/stdout"
--- aggregator/run.sh
+++ aggregator/run.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 echo "run aggregator revision [$GIT_REVISION]"
+
+export PGPASSWORD=$KPI_DATABASE_PASSWORD
+
+echo connect to [$KPI_DATABASE_HOST] with user [$KPI_DATABASE_USER] to database [$KPI_DATABASE_NAME]
+
+psql -h $KPI_DATABASE_HOST -U $KPI_DATABASE_USER -d $KPI_DATABASE_NAME -f /tmp/aggregator.sql
