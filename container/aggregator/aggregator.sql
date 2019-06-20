679882818b33e4b590103f811782860281ddc1d7
Richard Mörbitz <richard.moerbitz@tu-dresden.de>
2019-06-20 18:02:25 +0200

    aggregator: finish setup

--- container/aggregator/aggregator.sql
+++ container/aggregator/aggregator.sql
@@ -1,1 +1,6 @@
-SELECT max(id) FROM public.uuid;
+CREATE TABLE IF NOT EXISTS day (day date);
+
+INSERT INTO day
+SELECT i::date FROM GENERATE_SERIES((SELECT COALESCE(MAX(day), '2013-12-31')FROM day) + 1,
+                                    CURRENT_DATE,
+                                    '1 day'::interval) i;
--- container/aggregator/run.sh
+++ container/aggregator/run.sh
@@ -4,6 +4,6 @@ echo "run aggregator revision [$GIT_REVISION]"
 
 export PGPASSWORD=$KPI_DATABASE_PASSWORD
 
-echo connect to [$KPI_DATABASE_HOST] with user [$KPI_DATABASE_USER] to database [$KPI_DATABASE_NAME]
+echo connect to [$KPI_DATABASE_HOST:$KPI_DATABASE_PORT] with user [$KPI_DATABASE_USER] to database [$KPI_DATABASE_NAME] password [$PGPASSWORD]
 
-psql -h $KPI_DATABASE_HOST -U $KPI_DATABASE_USER -d $KPI_DATABASE_NAME -f /tmp/aggregator.sql
+psql -h $KPI_DATABASE_HOST -U $KPI_DATABASE_USER -d $KPI_DATABASE_NAME -p $KPI_DATABASE_PORT -f /tmp/aggregator.sql
