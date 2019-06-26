1b67dda74666edb3b66aca443892875a20d39e83
Richard Bock <richard.bock@rbock44.de>
2019-06-26 03:22:45 +0200

    FIX: do not display password in run.sh

--- container/aggregator/run.sh
+++ container/aggregator/run.sh
@@ -4,6 +4,6 @@ echo "run aggregator revision [$GIT_REVISION]"
 
 export PGPASSWORD=$KPI_DATABASE_PASSWORD
 
-echo connect to [$KPI_DATABASE_HOST:$KPI_DATABASE_PORT] with user [$KPI_DATABASE_USER] to database [$KPI_DATABASE_NAME] password [$PGPASSWORD]
+echo connect to [$KPI_DATABASE_HOST:$KPI_DATABASE_PORT] with user [$KPI_DATABASE_USER] to database [$KPI_DATABASE_NAME]
 
 psql -h $KPI_DATABASE_HOST -U $KPI_DATABASE_USER -d $KPI_DATABASE_NAME -p $KPI_DATABASE_PORT -f /tmp/aggregator.sql
