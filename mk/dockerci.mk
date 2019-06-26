d951a620d9a29a006901d6e936d89f6d5c2b8d19
Richard Bock <richard.bock@rbock44.de>
2019-06-26 12:10:08 +0200

    ADD: also only major version tag

--- mk/dockerci.mk
+++ mk/dockerci.mk
@@ -10,6 +10,10 @@ ifeq ($(local_image),)
 $(error local_image not defined)
 endif
 
+ifeq ($(major_version),)
+$(error major_version not defined)
+endif
+
 ifeq ($(minor_version),)
 $(error minor_version not defined)
 endif
@@ -21,10 +25,12 @@ gce_image := eu.gcr.io/serlo-containers/$(image_name)
 docker_push:
 	docker tag $(local_image):latest $(gce_image):latest
 	docker push $(gce_image):latest
-	docker tag $(local_image):latest $(gce_image):$(minor_version)
-	docker push $(gce_image):$(minor_version)
-	docker tag $(local_image):latest $(gce_image):$(minor_version).$(shell git log --pretty=format:'' | wc -l)
-	docker push $(gce_image):$(minor_version).$(shell git log --pretty=format:'' | wc -l)
+	docker tag $(local_image):latest $(gce_image):$(major_version)
+	docker push $(gce_image):$(major_version)
+	docker tag $(local_image):latest $(gce_image):$(major_version).$(minor_version)
+	docker push $(gce_image):$(major_version).$(minor_version)
+	docker tag $(local_image):latest $(gce_image):$(major_version).$(minor_version).$(shell git log --pretty=format:'' | wc -l)
+	docker push $(gce_image):$(major_version).$(minor_version).$(shell git log --pretty=format:'' | wc -l)
 	docker tag $(local_image):latest $(gce_image):sha-$(shell git describe --dirty --always)
 	docker push $(gce_image):sha-$(shell git describe --dirty --always)
 
