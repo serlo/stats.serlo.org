39437dbab3f4da4e76eabebe77a1d7b53b7892fe
Richard Bock <richard.bock@rbock44.de>
2019-06-26 11:29:42 +0200

    FIX: variables for versions

--- mk/dockerci.mk
+++ mk/dockerci.mk
@@ -10,13 +10,14 @@ ifeq ($(minor_version),)
 $(error minor_version not defined)
 endif
 
+gce_image := eu.gcr.io/serlo-containers/$(image_name)
+local_image := serlo/$(image_name)
+patch_version := $(minor_version).$(shell git log --pretty=format:'' | wc -l)
+revision := "$(shell git describe --dirty --always)"
+
 .PHONY: docker_push
 # push docker container to gcr.io registry
 docker_push:
-	gce_image := eu.gcr.io/serlo-containers/$(image_name)
-	local_image := serlo/$(image_name)
-	patch_version := $(minor_version).$(shell git log --pretty=format:'' | wc -l)
-	revision := "$(shell git describe --dirty --always)"
 	docker tag $(local_image):latest $(gce_image):latest
 	docker push $(gce_image):latest
 	docker tag $(local_image):latest $(gce_image):$(minor_version)
