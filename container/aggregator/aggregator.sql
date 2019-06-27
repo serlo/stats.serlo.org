bdb33ecf3f0eed42befc87920b26280871eede9e
Valentin Roland <valentin@vroland.de>
2019-06-27 22:22:43 +0200

    do not create aggregated event log

--- container/aggregator/aggregator.sql
+++ container/aggregator/aggregator.sql
@@ -1,12 +1,53 @@
+/* Create a day table for day aggregation. */
 CREATE TABLE IF NOT EXISTS day (day date);
-
 INSERT INTO day
 SELECT i::date FROM GENERATE_SERIES((SELECT COALESCE(MAX(day), '2013-12-31')FROM day) + 1,
                                     CURRENT_DATE,
                                     '1 day'::interval) i;
 
+/* Some metrics are expensive to aggregate daily, use weekly aggregation instead.*/
+CREATE TABLE IF NOT EXISTS week (week date);
+INSERT INTO week
+SELECT i::date FROM GENERATE_SERIES((SELECT COALESCE(MAX(week), '2013-12-31')FROM week) + 1,
+                                    CURRENT_DATE,
+                                    '1 week'::interval) i;
+
 CREATE INDEX IF NOT EXISTS date_idx ON event_log(date);
 
+CREATE TABLE IF NOT EXISTS cache_event_log_aggregate_weekly (week date) INHERITS (event_log);
+
+INSERT INTO cache_event_log_aggregate_weekly  (
+    SELECT * FROM event_log JOIN week ON  
+        date BETWEEN week - interval '90 day' AND week
+        AND date > (SELECT COALESCE(MAX(date), '1970-01-01') from cache_event_log_aggregate_weekly  )
+        AND week >= '2018-01-01'
+);
+
+DROP MATERIALIZED VIEW IF EXISTS cache_active_authors CASCADE;
+CREATE MATERIALIZED VIEW cache_active_authors AS (
+    SELECT 
+        day as "time",
+        count(author) as "authors",
+        count(author_active) as "active authors",
+        count(author_very_active) as "very active authors"
+    FROM (
+        SELECT day, actor_id,
+            actor_id as author,
+            CASE WHEN count(actor_id) > 10 THEN actor_id END as author_active,
+            CASE WHEN count(actor_id) > 100 THEN actor_id END as author_very_active
+        FROM event_log JOIN day ON 
+            date BETWEEN day - interval '90 day' and day
+            AND event_id IN (4,5)
+            AND day >= '2018-01-01'
+            AND day <= (SELECT MAX(date) FROM event_log)
+        GROUP BY day, actor_id
+    ) activity 
+    GROUP BY day 
+    ORDER BY day ASC
+);
+
+
+/*
 DROP MATERIALIZED VIEW IF EXISTS event_log_window_90 CASCADE;
 CREATE MATERIALIZED VIEW event_log_window_90 AS (
     SELECT * FROM 
@@ -18,4 +59,4 @@ CREATE MATERIALIZED VIEW event_log_window_90 AS (
         
 );
 
-CREATE INDEX date_window_90_idx ON event_log_window_90(date);
+*/
