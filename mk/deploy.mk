125cb76f54886c946c26956aa4d0b75bd1f897b1
Richard Bock <richard.bock@rbock44.de>
2019-07-03 23:39:32 +0200

    FIX: deploy method use terraform

--- mk/deploy.mk
+++ mk/deploy.mk
@@ -5,6 +5,9 @@
 # location of the current serlo database dump
 export dump_location ?= gs://serlo_dev_terraform/sql-dumps/dump-2019-05-13.zip
 
+resource_importer = module.kpi.kubernetes_deployment.mysql-importer-cronjob
+resource_aggregator = module.kpi.kubernetes_deployment.aggregator-cronjob
+
 # download the database dump
 tmp/dump.zip:
 	mkdir -p tmp
@@ -19,13 +22,12 @@ provide_athene2_content: tmp/dump.sql
 
 .PHONY: deploy_aggregator
 # force the deployment of the aggregator
-deploy_aggregator: kubectl_use_context
-	kubectl patch deployment aggregator-cronjob --namespace kpi  -p "{\"spec\": {\"template\": {\"metadata\": { \"labels\": {  \"redeploy\": \"$$(date +%s)\"}}}}}"
+deploy_aggregator:
+	cd $(infrastructure_repository)/$(env_folder) && terraform taint $(resource_aggregator) && $(MAKE) terraform_apply
 
 .PONY: deploy_importer
 # force the deployment of the mysql importer
-deploy_importer: kubectl_use_context
-	kubectl patch deployment mysql-importer-cronjob --namespace kpi  -p "{\"spec\": {\"template\": {\"metadata\": { \"labels\": {  \"redeploy\": \"$$(date +%s)\"}}}}}"
-
+deploy_importer:
+	cd $(infrastructure_repository)/$(env_folder) && terraform taint $(resource_importer) && $(MAKE) terraform_apply
 
 .NOTPARALLEL:
