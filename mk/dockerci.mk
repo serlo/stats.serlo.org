11c44db2131d3a95e345633813e1c86566d1c212
Richard Bock <richard.bock@rbock44.de>
2019-07-03 22:25:27 +0200

    REF: serlo-containers changed to serlo-shared in gcr.io

--- mk/dockerci.mk
+++ mk/dockerci.mk
@@ -18,7 +18,17 @@ ifeq ($(minor_version),)
 $(error minor_version not defined)
 endif
 
-gce_image := eu.gcr.io/serlo-containers/$(image_name)
+gce_image := eu.gcr.io/serlo-shared/$(image_name)
+
+.PHONY: docker_build_ci
+# builds the docker image in the ci and pushes it to eu.gcr.io
+docker_build_ci:
+	 docker pull $(gce_image):$(version) 2>/dev/null >/dev/null || $(MAKE) docker_build docker_push
+
+.PHONY: docker_build_minikube
+# checks if the docker images is in the remote docker and builds it if not
+docker_build_minikube:
+	docker images | grep $(local_image) && echo "image $(local_image) already exists use docker_build" || $(MAKE) docker_build
 
 .PHONY: docker_push
 # push docker container to gcr.io registry
