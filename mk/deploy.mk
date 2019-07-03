270b7919f2d31708dbb3602c0c713458c72cfef1
Richard Bock <richard.bock@rbock44.de>
2019-07-04 01:03:25 +0200

    ADD: deploy grafana goal

--- mk/deploy.mk
+++ mk/deploy.mk
@@ -7,6 +7,7 @@ export dump_location ?= gs://serlo_dev_terraform/sql-dumps/dump-2019-05-13.zip
 
 resource_importer = module.kpi.kubernetes_deployment.mysql-importer-cronjob
 resource_aggregator = module.kpi.kubernetes_deployment.aggregator-cronjob
+resource_grafana = module.kpi.kubernetes_deployment.grafana_deployment
 
 # download the database dump
 tmp/dump.zip:
@@ -29,5 +30,10 @@ deploy_aggregator:
 # force the deployment of the mysql importer
 deploy_importer:
 	cd $(infrastructure_repository)/$(env_folder) && terraform taint $(resource_importer) && $(MAKE) terraform_apply
+
+.PONY: deploy_grafana
+# force the deployment of grafana
+deploy_grafana:
+	cd $(infrastructure_repository)/$(env_folder) && terraform taint $(resource_grafana) && $(MAKE) terraform_apply
 
 .NOTPARALLEL:
