d4195d3c77be9f3ac23cc1519ed65680e2301632
Valentin Roland <valentin@vroland.de>
2019-08-18 21:02:58 +0200

    simplify docker minikube setup

--- scripts/docker-setup-minikube.sh
+++ scripts/docker-setup-minikube.sh
@@ -10,15 +10,6 @@ if [ "$image" = "" ] ; then
     exit 1
 fi
 
-
-find_image() {
-    if [ "$tag" = "latest" ] ; then
-        docker images | grep -F $image
-    else
-        echo "Bla"
-    fi
-}
-
 eval $(minikube docker-env)
 greptag=$(echo $tag | sed s/latest//g)
 available=$(docker images | (grep -F "$image" || echo "") | (grep -F "$greptag" || echo ""))
@@ -28,7 +19,5 @@ if [ ! -z "$available" ] ; then
 fi
 
 docker pull $image:$tag
-docker save $image:$tag >tmp.image
-docker load <tmp.image
 eval $(minikube docker-env -u)
 rm -f tmp.image
