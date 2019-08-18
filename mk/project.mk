9d198edcc24987694395582a32ef731d8dcd67ac
Valentin Roland <valentin@vroland.de>
2019-08-18 19:37:00 +0200

    more make-like project_start target

--- mk/project.mk
+++ mk/project.mk
@@ -1,19 +1,17 @@
-.PHONY: project_create
+
+.PHONY: project_start
 # create a project minikube cluster and deploy the project resources,
 # all in one target.
+project_start: project_deploy project_launch
+
+ADDITIONAL_STEPS  := 
 ifeq ($(env_name),minikube)
-project_create: minikube_create project_deploy project_launch
-else
-project_create: project_deploy project_launch
+	ADDITIONAL_STEPS := minikube_start docker_minikube_setup
 endif
 
 .PHONY: project_deploy
 # deploy the project to an already running cluster
-ifeq ($(env_name),minikube)
-project_deploy: docker_minikube_setup terraform_apply grafana_restore_dashboards grafana_add_default_users grafana_set_preferences
-else
-project_deploy: terraform_apply provide_athene2_content grafana_restore_dashboards grafana_add_default_users grafana_set_preferences
-endif
+project_deploy: $(ADDITIONAL_STEPS) terraform_init terraform_apply grafana_restore_dashboards grafana_add_default_users grafana_set_preferences
 
 .PHONY: project_launch
 # launch the grafana dashboard
--- scripts/docker-setup-minikube.sh
+++ scripts/docker-setup-minikube.sh
@@ -1,29 +1,34 @@
 #!/bin/sh
 
+set -e
+
 image="$1"
 tag="$2"
 
 if [ "$image" = "" ] ; then
-    echo script requires image name
+    echo "script requires image name"
     exit 1
 fi
 
-eval $(minikube docker-env)
-if [ "$tag" = "latest" ] ; then
-    docker images | grep -F $image
-else
-    docker images | grep -F $image | grep -F $tag
-fi
 
-if [ $? = 0 ] ; then
+find_image() {
+    if [ "$tag" = "latest" ] ; then
+        docker images | grep -F $image
+    else
+        echo "Bla"
+    fi
+}
+
+eval $(minikube docker-env)
+greptag=$(echo $tag | sed s/latest//g)
+available=$(docker images | (grep -F "$image" || echo "") | (grep -F "$greptag" || echo ""))
+if [ ! -z "$available" ] ; then
    echo "nothing to do image $image:$tag available"
    exit 0 
 fi
 
-eval $(minikube docker-env -u)
 docker pull $image:$tag
 docker save $image:$tag >tmp.image
-eval $(minikube docker-env)
 docker load <tmp.image
 eval $(minikube docker-env -u)
 rm -f tmp.image
