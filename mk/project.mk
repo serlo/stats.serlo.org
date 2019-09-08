0dc500b823898a1dad5113315333b60bd87d3770
Stephan Kulla <git.mail@kulla.me>
2019-09-08 18:22:21 +0200

    make: Format mk/project.mk

--- mk/project.mk
+++ mk/project.mk
@@ -1,10 +1,16 @@
+#
+# Targets for the KPI project
+#
+
 .PHONY: project_start
-# create a project minikube cluster and deploy the project resources, all in one target.
-project_start: minikube_start docker_minikube_setup build_local project_deploy minikube_launch
+# create a project minikube cluster and deploy the project resources, all in
+# one target.
+project_start: minikube_start docker_minikube_setup build_local project_deploy \
+	minikube_launch
 
 .PHONY: project_deploy
 # deploy the project to an already running cluster
-project_deploy: docker_minikube_setup terraform_init terraform_apply 
+project_deploy: docker_minikube_setup terraform_init terraform_apply
 
 .PHONY: project_smoketest
 # run smoketest for kpi project
