b13b1e5920633bcf010e63178b344137cdee1650
Stephan Kulla <git.mail@kulla.me>
2019-09-08 18:49:45 +0200

    make: Add target "gcloud_login"

--- mk/gcloud.mk
+++ mk/gcloud.mk
@@ -2,9 +2,17 @@
 # Targets concerning the gcloud
 #
 
+GCLOUD_PROJECT := serlo-dev
+
 define GCLOUD_IS_LOGGED_OUT
 	gcloud auth list 2>&1 | grep "No credentialed accounts." > /dev/null
 endef
+
+.PHONY: gcloud_login
+# Logs in into gcloud
+gcloud_login:
+	if $(GCLOUD_IS_LOGGED_OUT); then gcloud auth login; fi
+	gcloud config set project $(GCLOUD_PROJECT)
 
 .PHONY: gcloud_logout
 # logs out from gcloud
