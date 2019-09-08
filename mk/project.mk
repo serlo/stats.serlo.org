32b92fe0c6f5da26a6b269947bba1112314007bf
Stephan Kulla <git.mail@kulla.me>
2019-09-08 21:17:38 +0200

    make: Update prerequisites of project_start

--- mk/project.mk
+++ mk/project.mk
@@ -5,8 +5,7 @@
 .PHONY: project_start
 # create a project minikube cluster and deploy the project resources, all in
 # one target.
-project_start: minikube_start docker_minikube_setup build_local project_deploy \
-	minikube_launch
+project_start: minikube_start build_local project_deploy minikube_launch
 
 .PHONY: project_deploy
 # deploy the project to an already running cluster
