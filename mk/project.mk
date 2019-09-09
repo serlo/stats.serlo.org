e428a32fd06d0bca1cb54d305e191a2203bf9a7d
Stephan Kulla <git.mail@kulla.me>
2019-09-09 09:33:30 +0200

    make: Move prerequisite from project_start to project_deploy

--- mk/project.mk
+++ mk/project.mk
@@ -5,11 +5,12 @@
 .PHONY: project_start
 # create a project minikube cluster and deploy the project resources, all in
 # one target.
-project_start: minikube_start build_local project_deploy minikube_launch
+project_start: minikube_start build_local docker_minikube_setup project_deploy \
+	minikube_launch
 
 .PHONY: project_deploy
 # deploy the project to an already running cluster
-project_deploy: docker_minikube_setup terraform_init terraform_apply
+project_deploy: terraform_init terraform_apply
 
 .PHONY: project_smoketest
 # run smoketest for kpi project
