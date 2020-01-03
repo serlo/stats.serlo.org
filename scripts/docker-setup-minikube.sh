6a3a16fa50eabb8d399abdb92a5baec2a4be42bc
Richard Mörbitz <richard.moerbitz@tu-dresden.de>
2020-01-03 11:47:57 +0100

    scripts: remove check from docker-setup-minikube

--- scripts/docker-setup-minikube.sh
+++ scripts/docker-setup-minikube.sh
@@ -11,12 +11,5 @@ if [ "$image" = "" ] ; then
 fi
 
 eval $(minikube docker-env)
-greptag=$(echo $tag | sed s/latest//g)
-available=$(docker images | (grep -F "$image" || echo "") | (grep -F "$greptag" || echo ""))
-if [ ! -z "$available" ] ; then
-   echo "nothing to do image $image:$tag available"
-   exit 0 
-fi
-
 docker pull $image:$tag
 eval $(minikube docker-env -u)
